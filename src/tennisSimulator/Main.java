7d23f0d00ee4b7832d9e30af499320ccade01ec1
marekk1992 <m.komaisko@gmail.com>
2022-03-29 00:04:31 +0300

    added main class of the console, which starts the app

---
+++ src/tennisSimulator/Main.java
@@ -0,0 +1,10 @@
+package tennisSimulator;
+
+public class Main {
+
+    private static Menu menu = new Menu();
+
+    public static void main(String[] args) {
+        menu.runTennisSimulator();
+    }
+}
